724c5ac5e01e5991c9919004ecd1580020387c37
martinglzv7 <mglzv7@gmail.com>
2017-02-19 19:27:35 -0600

    Update hola.c

--- hola.c
+++ hola.c
@@ -2,5 +2,8 @@
 
 int main(){
 	printf("Hola mundo");
+	printf(“Martín Alberto González Vázquez”);
+	printf(“Programar es facil”); 
+ 	printf(“Voy a compartir con mi compañero el codigo”);  
 	return 0;
 }
